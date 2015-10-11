03b7dc5871e5e460f25ad04cfa362937898b1a4e
Paulo Cunha <paulomrcunha@gmail.com>
2015-10-11 02:35:21 +0200

    checked after the styleguide

--- js/app.js
+++ js/app.js
@@ -11,22 +11,20 @@ var Enemy = function(row, speed) {
     // Each row has 83 pixels, but there is a misalignement of 30px due to the "3D effect" of the blocks
     this.y = row * 83 - 30;
     this.speed = speed;
-    
 };
 
 Enemy.prototype.checkCollision = function() {
-    if(
+    if (
         this.row == player.row &&
-        // Check if any point in the width of the player sprite 
+        // Check if any point in the width of the player sprite
         // matches any point in the width of the enemy sprite
         player.col * 101 + 25 < this.x + 101 && //Added 25px to adjust for the transparent areas of the sprite
         (player.col + 1) * 101 - 25 > this.x //Subtracted 25px to adjust for the transparent areas of the sprite
-        ) 
-    {
+    ) {
         lifeCounter.loseLife();
         player.reset();
         this.speed /= 1.05; //When the player loses, the speed is reduced to make the game easier
-    };
+    }
 };
 
 // Update the enemy's position, required method for game
@@ -35,10 +33,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-        this.x = this.x + this.speed * dt;
-        if(this.x > 505) {
-            this.x = -50; //This value allows the image to "enter" the scene, instead of just appearing there
-        };
+    this.x = this.x + this.speed * dt;
+    if(this.x > 505) {
+        this.x = -50; //This value allows the image to "enter" the scene, instead of just appearing there
+    }
     this.render();
     this.checkCollision();
 };
@@ -53,10 +51,9 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-    
     this.sprite = 'images/char-boy.png';
     this.col = 2;
-    this.row = 5; 
+    this.row = 5;
 };
 
 Player.prototype.checkWin = function() {
@@ -68,7 +65,7 @@ Player.prototype.checkWin = function() {
         allEnemies.forEach(function(enemy) {
             enemy.speed += 100;
         });
-    };
+    }
 };
 
 Player.prototype.update = function() {
@@ -77,12 +74,11 @@ Player.prototype.update = function() {
     this.y = this.row * 83 - 30;
     this.render();
     this.checkWin();
-
-    };
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
+};
 
 Player.prototype.handleInput = function(key) {
     if(key == 'left' && this.col > 0) {
@@ -96,7 +92,7 @@ Player.prototype.handleInput = function(key) {
     }
     else if(key == 'down' && this.row < 5) {
         this.row++;
-    };
+    }
 };
 
 Player.prototype.reset = function() {
@@ -104,8 +100,6 @@ Player.prototype.reset = function() {
     this.row = 5;
     allBonuses = generateBonuses();
 };
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -134,7 +128,7 @@ lifeCounter.render = function() {
         var y = this.row * 97;
         var x = (this.col - i) * 40 + 303;
         ctx.drawImage(Resources.get(lifeCounter.sprite), x, y);
-    };
+    }
 };
 
 lifeCounter.loseLife = function() {
@@ -143,12 +137,10 @@ lifeCounter.loseLife = function() {
     if(this.currentlives > 0) {
         console.log(this.currentlives);
         this.render;
-    }
-    else {
+    } else {
         console.log('Game Over');
         bolGameOver = true;
-    };
-    
+    }
 };
 
 //===============GAME OVER===================//
@@ -167,7 +159,7 @@ var gameOver = function() {
             bolGameOver = false;
             lifeCounter.currentlives = 3;
             score = 0;
-        };
+        }
     });
     return true;
 };
@@ -210,10 +202,8 @@ Bonus.prototype.checkBonus = function() {
         score += this.bonusValue;
         renderScore();
         allBonuses[1] = createBonus(pickGemColor());
-    };
+    }
 };
-
-
 
 //Probablility of the type of gem
 var pickGemColor = function() {
@@ -226,7 +216,7 @@ var pickGemColor = function() {
     }
     else {
         return 'greenGem';
-    };
+    }
 };
 
 //Helper functions
@@ -238,7 +228,6 @@ var getRandomInt = function(min, max) {
 var clearCanvas = function() {
     ctx.clearRect(0,0, 505, 585);
 };
-
 
 var createBonus = function(type) {
     switch(type) {
@@ -254,7 +243,7 @@ var createBonus = function(type) {
         case 'greenGem':
             return greenGem = new Bonus('images/Gem Green.png', getRandomInt(1,3), 500);
             break;
-    };
+    }
 };
 
 // Now instantiate your objects.
